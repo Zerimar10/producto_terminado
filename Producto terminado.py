bac85f898acbf494c569ef7225b7aa199f520f34
Zerimar10 <tartarushell@hotmail.com>
2025-12-12 10:50:38 -0700

    Update Producto terminado.py

--- Producto terminado.py
+++ Producto terminado.py
@@ -3,14 +3,6 @@ import smartsheet
 from datetime import datetime, timedelta
 import pandas as pd
 
-fecha_hora = datetime.now() - timedelta(hours=7)
-
-cells = []
-
-cells.append({
-    "columnId": COL_ID["fecha_hora"], "value": fecha_hora.strftime("%y-%m-%d %H: %M: %S")
-})
-
 st.set_page_config(page_title="Registro de Producto Terminado", layout="wide")
 
 ALMACEN_PASSWORD = st.secrets["ALMACEN_PASSWORD"]
@@ -422,6 +414,7 @@ with tab2:
         except Exception as e:
             st.error("❌ Error al guardar los cambios")
             st.write(e)
+
 
 
 
